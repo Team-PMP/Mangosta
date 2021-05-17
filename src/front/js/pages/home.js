6f2968d7041546fb3ba6d8731c18220db6c1642f
Miguel Angel Carvajal Gonzalez <amcarvajal15@gmail.com>
2021-05-17 17:52:15 +0000

    Primer comit

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,6 +1,5 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 
 export const Home = () => {
@@ -8,9 +7,9 @@ export const Home = () => {
 
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
+			<h1>TEAM PMP</h1>
 			<p>
-				<img src={rigoImageUrl} />
+				<img src="" />
 			</p>
 			<div className="alert alert-info">{store.message || "Loading message from the backend..."}</div>
 			<p>
