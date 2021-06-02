383adfce5330240f090d2b56c23a1e8d52c6255a
PATRICIAAntolin <prothings2021@gmail.com>
2021-06-02 19:27:50 +0000

    html a falta de img

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -6,72 +6,87 @@ export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-            <div className="header container">
-                <div className="information">
-                    <h1>¿Quienes somos?</h1>
-                    <p>Somos el google de la enfermedades autoinmunes, una red social que acerca los profesionales a los que estan en busqueda de ayuda tan valiosa como la suya. </p>
-                    <p>Nos encargamos que todos nuestros usuarios ya sea profesional o paciente, tenga un trato rapido, eficaz y staisfastorio.</p>
-                </div>
-                <div className="img information">
-                    <img src="">
-                </div>
-            </div>
+		<>
+			<div className="header container">
+				<div className="information">
+					<h1>¿Quienes somos?</h1>
+					<p>
+						Somos el google de la enfermedades autoinmunes, una red social que acerca los profesionales a
+						los que estan en busqueda de ayuda tan valiosa como la suya.{" "}
+					</p>
+					<p>
+						Nos encargamos que todos nuestros usuarios ya sea profesional o paciente, tenga un trato rapido,
+						eficaz y staisfastorio.
+					</p>
+				</div>
+				<div className="img information">
+					<img src="" />
+				</div>
+			</div>
 
-            <div className="work container">
-                <div classname="statement">
-                    <h2>¿Nuevo en Autodoctor?</h2>
-                    <p>Conecta con los mejores profesionales y la mejor comunidad en unos sencillos pasos</p>
-                </div>
-                <div class="card-group">
-                    <div class="card">
-                        <img class="card-img-top" src="..." alt="Card image cap">
-                        <div class="card-body">
-                             <p class="card-text">Create una cuenta o haz login.</p>
-                        </div>
-                    </div>
-                    <div class="card">
-                         <img class="card-img-top" src="..." alt="Card image cap">
-                         <div class="card-body">
-                             <p class="card-text">Encuentra el profesional que más se adapte a tí.</p>
-                         </div>
-                    </div>
-                    <div class="card">
-                        <img class="card-img-top" src="..." alt="Card image cap">
-                        <div class="card-body">
-                             <p class="card-text">Te ayudaran con tus problemas.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+			<div className="work container">
+				<div className="statement">
+					<h2>¿Nuevo en Autodoctor?</h2>
+					<p>Conecta con los mejores profesionales y la mejor comunidad en unos sencillos pasos</p>
+				</div>
+				<div className="card-group">
+					<div className="card">
+						<img className="card-img-top" src="..." alt="Card image cap" />
+						<div className="card-body">
+							<p className="card-text">Create una cuenta o haz login.</p>
+						</div>
+					</div>
+					<div className="card">
+						<img className="card-img-top" src="..." alt="Card image cap" />
+						<div className="card-body">
+							<p className="card-text">Encuentra el profesional que más se adapte a tí.</p>
+						</div>
+					</div>
+					<div className="card">
+						<img className="card-img-top" src="..." alt="Card image cap" />
+						<div className="card-body">
+							<p className="card-text">Te ayudaran con tus problemas.</p>
+						</div>
+					</div>
+				</div>
+			</div>
 
-            <div className="resources container">
-                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                     <ol class="carousel-indicators">
-                         <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                         <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                         <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                     </ol>
-                     <div class="carousel-inner">
-                        <div class="carousel-item active">
-                             <img class="d-block w-100" src="..." alt="First slide">
-                        </div>
-                        <div class="carousel-item">
-                             <img class="d-block w-100" src="..." alt="Second slide">
-                        </div>
-                        <div class="carousel-item">
-                            <img class="d-block w-100" src="..." alt="Third slide">
-                        </div>
-                     </div>
-                     <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                     </a>
-                     <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                         <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                         <span class="sr-only">Next</span>
-                     </a>
-                </div>
-            </div>
-        
+			<div className="resources container">
+				<div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+					<ol className="carousel-indicators">
+						<li data-target="#carouselExampleIndicators" data-slide-to="0" className="active" />
+						<li data-target="#carouselExampleIndicators" data-slide-to="1" />
+						<li data-target="#carouselExampleIndicators" data-slide-to="2" />
+					</ol>
+					<div className="carousel-inner">
+						<div className="carousel-item active">
+							<img className="d-block w-100" src="..." alt="First slide" />
+						</div>
+						<div className="carousel-item">
+							<img className="d-block w-100" src="..." alt="Second slide" />
+						</div>
+						<div className="carousel-item">
+							<img className="d-block w-100" src="..." alt="Third slide" />
+						</div>
+					</div>
+					<a
+						className="carousel-control-prev"
+						href="#carouselExampleIndicators"
+						role="button"
+						data-slide="prev">
+						<span className="carousel-control-prev-icon" aria-hidden="true" />
+						<span className="sr-only">Previous</span>
+					</a>
+					<a
+						className="carousel-control-next"
+						href="#carouselExampleIndicators"
+						role="button"
+						data-slide="next">
+						<span className="carousel-control-next-icon" aria-hidden="true" />
+						<span className="sr-only">Next</span>
+					</a>
+				</div>
+			</div>
+		</>
 	);
 };
