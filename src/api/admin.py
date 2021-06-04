68f0c0dc90812428a18eb4b016e4b105a04c820b
mangostaninja <piuquen.t@gmail.com>
2021-06-04 17:06:02 +0000

    import more tables

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User, Service, Post
+from .models import db, User, Service, Post, Specialty, Disease, Comment
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -14,6 +14,9 @@ def setup_admin(app):
     admin.add_view(ModelView(User, db.session))
     admin.add_view(ModelView(Service, db.session))
     admin.add_view(ModelView(Post, db.session))
+    admin.add_view(ModelView(Specialty, db.session))
+    admin.add_view(ModelView(Disease, db.session))
+    admin.add_view(ModelView(Comment, db.session))
 
 
     # You can duplicate that line to add mew models
