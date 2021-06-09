c8cf261be4ebccbe5cda45b367ad4c2e18341561
PATRICIAAntolin <prothings2021@gmail.com>
2021-06-09 12:27:41 +0000

    falta endpoint

--- src/front/js/component/registro/registro.js
+++ src/front/js/component/registro/registro.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form, Col, Row, Container, Button } from "react-bootstrap";
 
 export const Registro = () => {
 	const [username, setUsername] = useState("");
@@ -10,115 +11,217 @@ export const Registro = () => {
 	const [specialty, setSpecialty] = useState("");
 	const [services, setServices] = useState("");
 
+	const [formData, setFormData] = useState({
+		username: "",
+		name: "",
+		surname: "",
+		email: "",
+		password: "",
+		repeatPassword: "",
+		specialty: "",
+		services: ""
+	});
+
+	// const handleChange = evt => {
+	// 	setFormData({
+	// 		[evt.target.name]: evt.target.value
+	// 	});
+	// };
+
+	const handleSubmit = evt => {
+		evt.preventDefault();
+		setFormData({
+			username: username,
+			name: name,
+			surname: surname,
+			email: email,
+			password: password,
+			repeatPassword: repeatPassword,
+			specialty: specialty,
+			services: services
+		});
+
+		console.log(formData);
+	};
+
 	return (
-		<form>
-			<fieldset className="form-group">
-				<div className="row">
-					<label htmlFor="typeOfUser">Tipo de usuario</label>
-					<input type="radio" name="typeOfUser" value="Pro" />
-					Profesional
-					<input type="radio" name="typeOfUser" value="U" />
-					Usuario
-				</div>
-			</fieldset>
+		<Container>
+			<Form className="mt-5" onSubmit={handleSubmit}>
+				{/* Tipo de usuario */}
+				<fieldset>
+					<Form.Group as={Row} className="align-items-center">
+						<Form.Label as="legend" column sm={2}>
+							Tipo de Usuario
+						</Form.Label>
+						<Row sm={10} className="ml-3">
+							<Form.Check type="radio" label="Profesional" name="typeOfUser" value="Pro" />
+							<Form.Check type="radio" label="Usuario" name="typeOfUser" value="U" className="ml-4" />
+						</Row>
+					</Form.Group>
+				</fieldset>
 
-			<div>
-				<label htmlFor="username">Nombre de usuario</label>
-				<input
-					value={username}
-					onChange={event => setUsername(event.target.value)}
-					name="username"
-					id="username"
-					type="text"
-				/>
-			</div>
+				{/* Nombre de usuario */}
+				<Form.Group as={Row}>
+					<Form.Label column sm={2} htmlFor="username">
+						Nombre de Usuario
+					</Form.Label>
+					<Col sm={10}>
+						<Form.Control
+							type="text"
+							placeholder="Nombre de Usuario"
+							name="username"
+							id="username"
+							value={username}
+							onChange={event => setUsername(event.target.value)}
+						/>
+					</Col>
+				</Form.Group>
 
-			<div>
-				<label htmlFor="name">Nombre</label>
-				<input value={name} onChange={event => setName(event.target.value)} name="name" id="name" type="text" />
-			</div>
+				{/* Nombre */}
+				<Form.Group as={Row}>
+					<Form.Label column sm={2} htmlFor="name">
+						Nombre
+					</Form.Label>
+					<Col sm={10}>
+						<Form.Control
+							type="text"
+							placeholder="Nombre"
+							name="name"
+							id="name"
+							value={name}
+							onChange={event => setName(event.target.value)}
+						/>
+					</Col>
+				</Form.Group>
 
-			<div>
-				<label htmlFor="surname">Apellidos</label>
-				<input
-					value={surname}
-					onChange={event => setSurname(event.target.value)}
-					name="surname"
-					id="surname"
-					type="text"
-				/>
-			</div>
+				{/* Apellidos */}
+				<Form.Group as={Row}>
+					<Form.Label column sm={2} htmlFor="surname">
+						Apellidos
+					</Form.Label>
+					<Col sm={10}>
+						<Form.Control
+							type="text"
+							placeholder="Apellidos"
+							name="surname"
+							id="surname"
+							value={surname}
+							onChange={event => setSurname(event.target.value)}
+						/>
+					</Col>
+				</Form.Group>
 
-			<div>
-				<label htmlFor="email">E-mail</label>
-				<input
-					value={email}
-					onChange={event => setEmail(event.target.value)}
-					name="email"
-					id="email"
-					type="text"
-				/>
-			</div>
+				{/* Email */}
+				<Form.Group as={Row}>
+					<Form.Label column sm={2} htmlFor="email">
+						E-mail
+					</Form.Label>
+					<Col sm={10}>
+						<Form.Control
+							type="email"
+							placeholder="E-mail"
+							name="email"
+							id="email"
+							value={email}
+							onChange={event => setEmail(event.target.value)}
+						/>
+					</Col>
+				</Form.Group>
 
-			<div>
-				<label htmlFor="password">Contraseña</label>
-				<input
-					value={password}
-					onChange={event => setPassword(event.target.value)}
-					name="password"
-					id="password"
-					type="password"
-				/>
-			</div>
+				{/* Contraseña */}
+				<Form.Group as={Row}>
+					<Form.Label column sm={2} htmlFor="password">
+						Contraseña
+					</Form.Label>
+					<Col sm={10}>
+						<Form.Control
+							type="password"
+							placeholder="Contraseña"
+							name="password"
+							id="password"
+							value={password}
+							onChange={event => setPassword(event.target.value)}
+						/>
+					</Col>
+				</Form.Group>
 
-			<div>
-				<label htmlFor="repeatPassword">Repetir contraseña</label>
-				<input
-					value={repeatPassword}
-					onChange={event => setRepeatPassword(event.target.value)}
-					name="repeatPassword"
-					id="repeatPassword"
-					type="password"
-				/>
-			</div>
+				{/* Repetir contraseña */}
+				<Form.Group as={Row}>
+					<Form.Label column sm={2} htmlFor="repeatPassword">
+						Repetir Contraseña
+					</Form.Label>
+					<Col sm={10}>
+						<Form.Control
+							type="password"
+							placeholder="Repetir Contraseña"
+							name="repeatPassword"
+							id="repeatPassword"
+							value={repeatPassword}
+							onChange={event => setRepeatPassword(event.target.value)}
+						/>
+					</Col>
+				</Form.Group>
 
-			<div>
-				<label htmlFor="specialty">Especialidad</label>
-				<select
-					id="specialty"
-					name="specialty"
-					value={specialty}
-					onChange={event => setSpecialty(event.target.value)}>
-					<option value="Esp">Seleccione una especialidad</option>
-					<option value="End">Endocrinología</option>
-					<option value="Inm">Inmunología</option>
-					<option value="Der">Dermatología</option>
-					<option value="Reu">Reumatología</option>
-				</select>
-			</div>
+				{/* Especialidad */}
+				<Form.Group as={Row}>
+					<Form.Label column sm={2} htmlFor="specialty">
+						Especialidad
+					</Form.Label>
+					<Col sm={10}>
+						<Form.Control
+							as="select"
+							id="specialty"
+							name="specialty"
+							value={specialty}
+							onChange={event => setSpecialty(event.target.value)}>
+							<option value="Esp">Seleccione una especialidad</option>
+							<option value="End">Endocrinología</option>
+							<option value="Inm">Inmunología</option>
+							<option value="Der">Dermatología</option>
+							<option value="Reu">Reumatología</option>
+						</Form.Control>
+					</Col>
+				</Form.Group>
 
-			<div>
-				<label htmlFor="services">Servicios</label>
-				<select
-					id="services"
-					name="services"
-					value={services}
-					onChange={event => setServices(event.target.value)}>
-					<option value="Ser">Seleccione un servicio</option>
-					<option value="Ac">Acupuntura</option>
-					<option value="Nat">Naturopatía</option>
-					<option value="MC">Medicina china</option>
-					<option value="Med">Meditación</option>
-				</select>
-			</div>
-
-			<div>
-				<input type="checkbox" id="acept" name="acept" value="" />
-				Acepto términos y condiciones
-			</div>
-			<div>
-				<button>Registrarse</button>
-			</div>
-		</form>
+				{/* Servicios */}
+				<Form.Group as={Row}>
+					<Form.Label column sm={2} htmlFor="services">
+						Servicios
+					</Form.Label>
+					<Col sm={10}>
+						<Form.Control
+							as="select"
+							id="services"
+							name="services"
+							value={services}
+							onChange={event => setServices(event.target.value)}>
+							<option value="Ser">Seleccione un servicio</option>
+							<option value="Ac">Acupuntura</option>
+							<option value="Nat">Naturopatía</option>
+							<option value="MC">Medicina china</option>
+							<option value="Med">Meditación</option>
+						</Form.Control>
+					</Col>
+				</Form.Group>
+				<Row className="justify-content-center mt-5">
+					{/* Terminos y condiciones */}
+					<Form.Group as={Row}>
+						<Form.Check
+							type="checkbox"
+							label="Acepto términos y condiciones"
+							id="accept"
+							name="accept"
+							value=""
+						/>
+					</Form.Group>
+				</Row>
+				<Row className="justify-content-center mt-4">
+					{/* Submit */}
+					<Button variant="primary" type="submit" className="align-items-center">
+						Registrarse
+					</Button>
+				</Row>
+			</Form>
+		</Container>
 	);
 };
