1757ee2b654616b5fd456e47386bfb8f8a3722aa
Miguel Angel Carvajal Gonzalez <amcarvajal15@gmail.com>
2021-07-05 17:04:05 +0000

    migrations fixed and Speciality DB fixed aswell

--- migrations/versions/f76d17d5f5b9_.py
+++ migrations/versions/f76d17d5f5b9_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 53c50852e0f7
+Revision ID: f76d17d5f5b9
 Revises: 
-Create Date: 2021-07-02 13:55:00.320671
+Create Date: 2021-07-05 17:02:02.239212
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '53c50852e0f7'
+revision = 'f76d17d5f5b9'
 down_revision = None
 branch_labels = None
 depends_on = None
